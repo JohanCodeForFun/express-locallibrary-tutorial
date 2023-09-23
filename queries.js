14157e31a5788e93ecc69a8ec4a38981fa93c105
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-09-23 20:39:35 +0200

    working API with CRUD operations on books table

--- queries.js
+++ queries.js
@@ -1,6 +1,4 @@
-// const { Client } = require('pg');
-// const pgp = require('pg-promise')();
-// const db = pgp(connectionString);
+// API for PostgreSQL database, books table
 
 const connectionString = process.env.DB_CONNECTION_STRING;
 const Pool = require('pg').Pool;
@@ -11,7 +9,7 @@ const pool = new Pool({
 const getBooks = async (req, res) => {
   try {
     console.log('getBooks');
-    const books = await pool.query('SELECT * FROM book ORDER BY id ASC');
+    const books = await pool.query('SELECT * FROM books ORDER BY id ASC');
     res.status(200).json(books.rows);
   } catch (error) {
     console.log(error);
@@ -21,7 +19,7 @@ const getBooks = async (req, res) => {
 const getBookById = async (req, res) => {
   try {
     const { id } = req.params;
-    const book = await pool.query('SELECT * FROM book WHERE id = $1', [id]);
+    const book = await pool.query('SELECT * FROM books WHERE book_id = $1', [id]);
     res.status(200).json(book.rows);
   } catch (error) {
     console.log(error);
@@ -30,8 +28,8 @@ const getBookById = async (req, res) => {
 
 const createBook = async (req, res) => {
   try {
-    const { title, genre } = req.body;
-    const newBook = await pool.query('INSERT INTO book (title, genre) VALUES ($1, $2) RETURNING *', [titel, genre]);
+    const { book_title, book_summary, book_isbn } = req.body;
+    const newBook = await pool.query('INSERT INTO books (book_title, book_summary, book_isbn) VALUES ($1, $2, $3) RETURNING *', [book_title, book_summary, book_isbn]);
     res.status(200).json(newBook.rows);
   } catch (error) {
     console.log(error);
@@ -41,9 +39,9 @@ const createBook = async (req, res) => {
 const updateBook = async (req, res) => {
   try {
     const { id } = req.params;
-    const { title, genre } = req.body;
-    const updatedBook = await pool.query('UPDATE book SET title = $1, genre = $2 WHERE id = $3', [title, genre, id]);
-    res.status(200).json(updatedBook.rows);
+    const { book_title, book_summary, book_isbn } = req.body;
+    const updatedBook = await pool.query('UPDATE books SET book_title = $1, book_summary = $2, book_isbn = $3 WHERE book_id = $4', [book_title, book_summary, book_isbn, id]);
+    res.status(200).json(updatedBook);
   } catch (error) {
     console.log(error);
   }
@@ -52,7 +50,7 @@ const updateBook = async (req, res) => {
 const deleteBook = async (req, res) => {
   try {
     const { id } = req.params;
-    const deletedBook = await pool.query('DELETE FROM book WHERE id = $1', [id]);
+    const deletedBook = await pool.query('DELETE FROM books WHERE book_id = $1', [id]);
     res.status(200).json(deletedBook.rows);
   } catch (error) {
     console.log(error);
