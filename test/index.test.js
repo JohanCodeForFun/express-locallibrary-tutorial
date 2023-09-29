e3151422533e7e106a54607d4e848c242b40ecdc
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-09-29 22:01:42 +0200

    update tests for index

--- test/index.test.js
+++ test/index.test.js
@@ -8,33 +8,45 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use("/", index);
 
-describe("testing index routes", () => {
+describe("testing catalog routes", () => {
   test("index route works", done => {
     request(app)
-      .get("/")
+      .get('/')
+      .expect(redirect("/catalog"))
       .expect(200, done);
   });
 
   test('expect title for home page', done => {
     request(app)
-      .get('/')
-      .expect('Content-Type', /html/)
-      .expect(/<title>Local Library<\/title>/)
+      .get('/catalog')
+      .expect('Content-Type', /text\/html;/)
+      .expect(/<title>Local Library Home<\/title>/)
       .expect(200, done)
   });
 
-  test('expect link to books page', done => {
+  test('expect navbar with links to exist', done => {
+      // write test to expect navbar to include ul element with class navbar-nav
+      // write test to expect navbar to include 10 li element with class nav-item
+
     request(app)
-      .get('/')
-      .expect('Content-Type', /html/)
-      .expect(/<a href="\/books">Books<\/a>/)
+      .get('/catalog')
+
+      .expect(/<a href="\/catalog">Home<\/a>/)
+      .expect(/<a href="\/catalog\/books">All Books<\/a>/)
+      .expect(/<a href="\/catalog\/authors">All Authors<\/a>/)
+      .expect(/<a href="\/catalog\/genres">All Genres<\/a>/)
+      .expect(/<a href="\/catalog\/bookinstances">All Book Instances<\/a>/)
+      .expect(/<a href="\/catalog\/book\/create">Create new author<\/a>/)
+      .expect(/<a href="\/catalog\/book\/create">Create new genre<\/a>/)
+      .expect(/<a href="\/catalog\/book\/create">Create new book<\/a>/)
+      .expect(/<a href="\/catalog\/book\/create">Create new book instance (copy)<\/a>/)
+
       .expect(200, done)
   });
 
   test('expect page to include author', done => {
     request(app)
-      .get('/')
-      .expect('Content-Type', /html/)
+      .get('/catalog')
       .expect(/Johan Hellberg/)
       .expect(200, done)
   });
